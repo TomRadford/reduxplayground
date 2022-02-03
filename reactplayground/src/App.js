3f20fccc0413732982bd797385fe5597df8cb0db
Tom Radford <tom@theradford.com>
2022-02-03 11:43:17 +0200

    new updates

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -1,38 +1,52 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 
-const App = (props) => {
-   const [notes, setNotes] = useState(props.notes)
-   const [newNote, setNewNote] = useState('new note...')
-   const [showAll, setShowAll] = useState(true)
 
-   const addNote = (event) => {
-       event.preventDefault()
-               const noteObject = {
+
+const App = (props) => {
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState('new note...')
+    const [showAll, setShowAll] = useState(true)
+    
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    },[])
+
+    console.log('render', notes.length, 'notes')
+
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
             id: notes.length + 1,
             content: newNote,
             date: new Date().toISOString,
             important: Math.random() > 0.5
-        } 
+        }
         setNotes(notes.concat(noteObject))
         setNewNote('')
-   }
+    }
 
-   const handleNoteChange = (event) => {
+    const handleNoteChange = (event) => {
         console.log(event.target.value)
         setNewNote(event.target.value)
-   }
+    }
 
-   const notesToShow = showAll 
-   ? notes 
-   : notes.filter(note => note.important)
-    
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
 
-   const handleFirstClick = () => 
-        {if (newNote === 'new note...') setNewNote('') }
-       
-   
-   
+
+    const handleFirstClick = () => { if (newNote === 'new note...') setNewNote('') }
+
+
+
     return (
         <div>
             <h1>Notes</h1>
@@ -42,15 +56,15 @@ const App = (props) => {
                 </button>
             </div>
             <ul>
-                {notesToShow.map(note => 
+                {notesToShow.map(note =>
                     <Note key={note.id} note={note} />
-                    )}
+                )}
             </ul>
             <form onSubmit={addNote} >
-                <input 
+                <input
                     onFocus={handleFirstClick}
                     value={newNote}
-                    onChange={handleNoteChange}/>
+                    onChange={handleNoteChange} />
                 <button type='submit'>Add</button>
             </form>
         </div>
