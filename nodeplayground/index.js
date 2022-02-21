8deaa6e78b3fbf830e70a06e3b12c605658a55d5
Tom Radford <tom@theradford.com>
2022-02-21 18:08:34 +0200

    'middleware'

--- nodeplayground/index.js
+++ nodeplayground/index.js
@@ -1,8 +1,24 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'Unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+  
 
 let notes = [
     {
@@ -53,29 +69,62 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note =>  note.id === id)
+    const note = notes.find(note => note.id === id)
     if (note) {
-        response.json(note)
+        response.json(notes)
+    
     } else {
         response.statusMessage = 'Note ID not found'
         response.status(404).end()
-    }    
+    }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
-
+    console.log(notes)
     response.status(204).end()
 })
 
+const generateId = () => {
+    maxId = notes.length > 0 
+    ? Math.max(...notes.map(note => note.id))
+    : 0 
+    return maxId + 1 
+}
+
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
-    response.json(note)
-})
+    if (request.is('application/json')) {
+               
+        const body = request.body
+
+        if (!body.content) {
+            return response.status(400).json({
+                error: 'content missing!'
+            })
+        }
+
+        const note = {
+            content: body.content,
+            important: body.important || false,
+            date: new Date(), 
+            id: generateId()
+        }
+        
+
+        notes = notes.concat(note)
+
+        console.log(notes)
+        response.json(note)
+
+    } else {response.status(400).json({error: 'Not JSON'})}
+}
+)
+
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`)
+    console.log(`Server running on port: ${PORT}`)
 })
